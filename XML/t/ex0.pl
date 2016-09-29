22de496eff3ea7d273729c0f00306757e70ceada
Jochen Hayek <Jochen+github-oss@Hayek.name>
2016-09-30 00:57:46 +0200

    Create ex0.pl

---
+++ XML/t/ex0.pl
@@ -0,0 +1,14 @@
+#! /usr/bin/perl -w
+
+use lib '/Users/johayek/Computers/Programming/Languages/Perl';
+
+use XML::Template;
+
+my $template = XML::Template->new(filename => 'ex0.tmpl');
+
+# fill in some parameters
+$template->param(HOME => $ENV{HOME});
+$template->param(PATH => $ENV{PATH});
+
+# send the obligatory Content-Type and print the template output
+print "Content-Type: text/html\n\n", $template->output;
